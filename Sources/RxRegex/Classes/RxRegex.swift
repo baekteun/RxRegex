f166741cf85ccad834d3a4651ef38720d15b2a83
baegteun <baegteun@gmail.com>
2022-01-03 14:12:38 +0900

    :zap: :: regex()
    support optional, delete do block

--- Sources/RxRegex/Classes/RxRegex.swift
+++ Sources/RxRegex/Classes/RxRegex.swift
@@ -10,15 +10,25 @@ import RxSwift
 
 public extension Observable where Element == String{
     func regex(_ pattern: String,_ options: NSRegularExpression.Options = []) -> Observable<Bool>{
-        do {
-            return map {
-                let regex = try NSRegularExpression(pattern: pattern, options: options)
-                if let _ = regex.firstMatch(in: $0, options: [], range: NSRange(location: 0, length: $0.count)){
-                    return true
-                }
-                return false
+        return map {
+            let regex = try NSRegularExpression(pattern: pattern, options: options)
+            if let _ = regex.firstMatch(in: $0, options: [], range: NSRange(location: 0, length: $0.count)){
+                return true
             }
+            return false
         }
-        
+    }
+}
+
+public extension Observable where Element == String?{
+    func regex(_ pattern: String,_ options: NSRegularExpression.Options = []) -> Observable<Bool>{
+        return map {
+            guard let `in` = $0 else { return false }
+            let regex = try NSRegularExpression(pattern: pattern, options: options)
+            if let _ = regex.firstMatch(in: `in`, options: [], range: NSRange(location: 0, length: `in`.count)){
+                return true
+            }
+            return false
+        }
     }
 }
--- Tests/RxRegexTests/RxRegexTests.swift
+++ Tests/RxRegexTests/RxRegexTests.swift
@@ -5,5 +5,9 @@
 //  Created by 최형우 on 2022/01/03.
 //
 
-import Foundation
+import XCTest
 
+
+final class RxRegexTests{
+    
+}
