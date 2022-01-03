b49d3c21bfed00e3d5e6ec890f94c69190860a41
baegteun <baegteun@gmail.com>
2022-01-03 14:08:40 +0900

    :zap: :: regex()
    create regex Function

--- Sources/RxRegex/Classes/RxRegex.swift
+++ Sources/RxRegex/Classes/RxRegex.swift
@@ -6,4 +6,19 @@
 //
 
 import Foundation
+import RxSwift
 
+public extension Observable where Element == String{
+    func regex(_ pattern: String,_ options: NSRegularExpression.Options = []) -> Observable<Bool>{
+        do {
+            return map {
+                let regex = try NSRegularExpression(pattern: pattern, options: options)
+                if let _ = regex.firstMatch(in: $0, options: [], range: NSRange(location: 0, length: $0.count)){
+                    return true
+                }
+                return false
+            }
+        }
+        
+    }
+}
